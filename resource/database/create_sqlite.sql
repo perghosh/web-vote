314475b6fdf53871fa6adfc911ef23a43dc86da7
Per Ghosh <per@gorep.se>
2026-02-14 15:02:08 +0100

    chore

--- resource/database/create_sqlite.sql
+++ resource/database/create_sqlite.sql
@@ -27,21 +27,20 @@ CREATE TABLE table_number (
 );
 
 -- CREATE TABLE TGroup, group codes
-CREATE TABLE "TGroup" (
-   "GroupK" INTEGER NOT NULL
+CREATE TABLE "TCodeGroup" (
+    "CodeGroupK" INTEGER PRIMARY KEY NOT NULL
    ,"FName" VARCHAR(200) NOT NULL
    ,"FDescription" VARCHAR(250)
    ,"FLabel" VARCHAR(100)
    ,"FTable" VARCHAR(200)
    ,"FMainTable" VARCHAR(200)
    ,"FSchema" VARCHAR(50)
-   ,CONSTRAINT "PK_TGroup_GroupK" PRIMARY KEY ("GroupK")
 );
 
 -- CREATE TABLE TBaseCode, base code is used to connect codes that has a common meaning.
-CREATE TABLE "TBaseCode" (
-   "BaseCodeK" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
-   ,"GroupK" INTEGER NOT NULL                                   -- fk to group
+CREATE TABLE "TCodeBase" (
+   "CodeBaseK" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
+   ,"CodeGroupK" INTEGER NOT NULL                               -- fk to group
    ,"CreatedD" TEXT
    ,"UpdateD" TEXT
    ,"FId" INTEGER                                               -- Id used if there is a need to control numbers
@@ -51,16 +50,16 @@ CREATE TABLE "TBaseCode" (
    ,"FRank" INTEGER                                             -- Ranking is for making system easier to work with, maybe you need parents in a tree but just some items are suitable for parents. Select those with codes that has some sort of ranking
    ,"FIdle" INTEGER DEFAULT 0
    ,"FDeleted" INTEGER DEFAULT 0
-   ,CONSTRAINT "FK_TBaseCode_GroupK" FOREIGN KEY ("GroupK") REFERENCES "TGroup"("GroupK") ON DELETE CASCADE
+   ,CONSTRAINT "FK_TCodeBase_CodeGroupK" FOREIGN KEY ("CodeGroupK") REFERENCES "TCodeGroup"("CodeGroupK") ON DELETE CASCADE
 );
 
-CREATE INDEX "I_TBaseCode_GroupK" ON "TBaseCode" ("GroupK");
+CREATE INDEX "I_TCodeBase_CodeGroupK" ON "TCodeBase" ("CodeGroupK");
 
 -- CREATE TABLE TCode, code is used as a lookup table for fields in other tables
 CREATE TABLE "TCode" (
     "CodeK" INTEGER PRIMARY KEY AUTOINCREMENT
-   ,"GroupK" INTEGER NOT NULL                -- fk to group
-   ,"BaseCodeK" INTEGER                      -- if connected to a common base code
+   ,"CodeGroupK" INTEGER NOT NULL            -- fk to group
+   ,"CodeBaseK" INTEGER                      -- if connected to a common base code
    ,"SuperK" INTEGER                         -- parent code
    ,"CreatedD" TEXT
    ,"UpdateD" TEXT
@@ -78,10 +77,10 @@ CREATE TABLE "TCode" (
    ,"FInteger1" INTEGER                      -- Custom integer
    ,"FNumber0" REAL                          -- Custom number, could be used for anything that system is used for
    ,"FText0" VARCHAR(100)                    -- Custom text, used for anything thats appropriate to system
-   ,CONSTRAINT "FK_TCode_GroupK" FOREIGN KEY ("GroupK") REFERENCES "TGroup"("GroupK") ON DELETE CASCADE
+   ,CONSTRAINT "FK_TCode_CodeGroupK" FOREIGN KEY ("CodeGroupK") REFERENCES "TCodeGroup"("CodeGroupK") ON DELETE CASCADE
 );
 
-CREATE INDEX "I_TCode_GroupK" ON "TCode" ("GroupK");
+CREATE INDEX "I_TCode_CodeGroupK" ON "TCode" ("CodeGroupK");
 
 
 /* Used to group users and other items to a Container entity */
@@ -93,10 +92,35 @@ CREATE TABLE TContainer (
     FDatabase   VARCHAR(100)
 );
 
+CREATE TABLE TOrganization (
+   OrganizationK BLOB PRIMARY KEY DEFAULT (randomblob(16)),
+   ParentK BLOB,                    -- Parent organization for hierarchical structure
+   SuperK BLOB,                     -- Owner organization when used in hierarchical structure
+   CreateD DATETIME,                -- when organization was created
+   UpdateD DATETIME,                -- last time organization was updated
+   TypeC INTEGER,                   -- Type of organization (e.g., company, department, team)
+   StateC INTEGER,                  -- State of organization (active, inactive, etc.)
+   ClassC INTEGER,                  -- Class/category of organization
+   FName VARCHAR(500),              -- organization name
+   FCode VARCHAR(100),              -- Short code/abbreviation for the organization
+   FDescription TEXT,               -- describe organization
+   FAddress TEXT,                   -- Physical address
+   FPhone VARCHAR(50),              -- Contact phone
+   FEmail VARCHAR(255),             -- Contact email
+   FWebsite VARCHAR(255),           -- Website URL
+   FTaxId VARCHAR(100),             -- Tax ID / Registration number
+   FDeleted INTEGER DEFAULT 0       -- if organization is deleted
+);
+
+-- Indexes for foreign keys and common lookups
+CREATE INDEX I_TOrganization_ParentK ON TOrganization (ParentK);
+CREATE INDEX I_TOrganization_SuperK ON TOrganization (SuperK);
+
 /* Used to store user information */
 CREATE TABLE TUser (
     UserK           BLOB PRIMARY KEY DEFAULT (randomblob(16)),
     ContainerK      INTEGER NOT NULL,
+    OrganizationK   BLOB,
     UserGroupK      INTEGER,
     CreateD         DATETIME,
     UpdateD         DATETIME,
@@ -123,6 +147,7 @@ CREATE TABLE TUser (
 
     -- Foreign Key
     CONSTRAINT FK_TUser_ContainerK FOREIGN KEY (ContainerK) REFERENCES TContainer(ContainerK) ON DELETE CASCADE ON UPDATE CASCADE
+    -- CONSTRAINT FK_TUser_OrganizationK FOREIGN KEY (OrganizationK) REFERENCES TOrganization(OrganizationK) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 CREATE INDEX I_TUser_ContainerK ON TUser(ContainerK);
@@ -255,7 +280,8 @@ INSERT INTO table_number (number, name, description) VALUES (101, 'TGroup', 'Gro
 INSERT INTO table_number (number, name, description) VALUES (102, 'TBaseCode', 'Base codes for common code meanings');
 INSERT INTO table_number (number, name, description) VALUES (103, 'TCode', 'Lookup codes for fields in other tables');
 INSERT INTO table_number (number, name, description) VALUES (200, 'TContainer', 'Groups users and items to a Container entity');
-INSERT INTO table_number (number, name, description) VALUES (300, 'TUser', 'User information table');
+INSERT INTO table_number (number, name, description) VALUES (300, 'TOrganization', 'Organization table');
+INSERT INTO table_number (number, name, description) VALUES (400, 'TUser', 'User information table');
 INSERT INTO table_number (number, name, description) VALUES (1060, 'TPoll', 'Main poll table');
 INSERT INTO table_number (number, name, description) VALUES (1070, 'TPollSection', 'Poll sections for organizing questions');
 INSERT INTO table_number (number, name, description) VALUES (1080, 'TPollComment', 'Comments on polls');
@@ -263,4 +289,77 @@ INSERT INTO table_number (number, name, description) VALUES (1090, 'TPollLimit',
 INSERT INTO table_number (number, name, description) VALUES (1100, 'TPollQuestion', 'Poll questions table');
 INSERT INTO table_number (number, name, description) VALUES (1110, 'TPollAnswer', 'Poll answers table');
 
-INSERT INTO TContainer ( ContainerK, FName ) VALUES( 1, "user" )
+
+-- TCodeGroup inserts for code groups
+-- Using numbering scheme: 10, 20, 30 for existing tables, then 100+ for new tables
+-- Format: CodeGroupK = (table_number from table_number table) + column offset
+-- Column offsets: TypeC=1, StateC=2, ClassC=3, ChartC=4, DisplayC=5, etc.
+
+-- TContainer codes (table_number 200)
+INSERT INTO TCodeGroup (CodeGroupK, FName, FDescription, FTable, FMainTable) VALUES
+    (2001, 'TContainer.TypeC', 'Container type codes', 'TContainer', 'TContainer');
+
+-- TOrganization codes (table_number 3001)
+INSERT INTO TCodeGroup (CodeGroupK, FName, FDescription, FTable, FMainTable) VALUES
+    (3001, 'TOrganization.TypeC', 'Organization type codes (Company, Department, Team, etc.)', 'TOrganization', 'TOrganization'),
+    (3002, 'TOrganization.StateC', 'Organization state codes (Active, Inactive, Suspended, etc.)', 'TOrganization', 'TOrganization'),
+    (3003, 'TOrganization.ClassC', 'Organization class/category codes', 'TOrganization', 'TOrganization');
+
+-- TUser codes (table_number 400)
+INSERT INTO TCodeGroup (CodeGroupK, FName, FDescription, FTable, FMainTable) VALUES
+    (4001, 'TUser.CountryC', 'Country codes for users', 'TUser', 'TUser'),
+    (4002, 'TUser.RoleC', 'User role codes (Admin, Manager, Employee, etc.)', 'TUser', 'TUser'),
+    (4003, 'TUser.PermissionS', 'User permission level codes', 'TUser', 'TUser');
+
+-- TPoll codes (table_number 1060)
+INSERT INTO TCodeGroup (CodeGroupK, FName, FDescription, FTable, FMainTable) VALUES
+    (10601, 'TPoll.TypeC', 'Poll type codes (Survey, Quiz, Voting, etc.)', 'TPoll', 'TPoll'),
+    (10602, 'TPoll.StateC', 'Poll state codes (Draft, Published, Closed, Archived, etc.)', 'TPoll', 'TPoll'),
+    (10603, 'TPoll.ClassC', 'Poll class/category codes', 'TPoll', 'TPoll'),
+    (10604, 'TPoll.ChartC', 'Poll chart presentation type codes', 'TPoll', 'TPoll'),
+    (10605, 'TPoll.DisplayC', 'Poll display type codes', 'TPoll', 'TPoll');
+
+-- TPollSection codes (table_number 1070)
+INSERT INTO TCodeGroup (CodeGroupK, FName, FDescription, FTable, FMainTable) VALUES
+    (10701, 'TPollSection.TypeC', 'Poll section type codes', 'TPollSection', 'TPoll');
+
+-- TPollComment codes (table_number 1080)
+INSERT INTO TCodeGroup (CodeGroupK, FName, FDescription, FTable, FMainTable) VALUES
+    (10801, 'TPollComment.FormatS', 'Comment format type codes', 'TPollComment', 'TPoll'),
+    (10802, 'TPollComment.TypeC', 'Comment type codes', 'TPollComment', 'TPoll');
+
+-- TPollLimit codes (table_number 1090)
+INSERT INTO TCodeGroup (CodeGroupK, FName, FDescription, FTable, FMainTable) VALUES
+    (10901, 'TPollLimit.limit_type', 'Poll limit type codes', 'TPollLimit', 'TPoll');
+
+-- TPollQuestion codes (table_number 1100)
+INSERT INTO TCodeGroup (CodeGroupK, FName, FDescription, FTable, FMainTable) VALUES
+    (11001, 'TPollQuestion.TypeC', 'Poll question type codes (Single choice, Multiple choice, Text, etc.)', 'TPollQuestion', 'TPoll'),
+    (11002, 'TPollQuestion.StateC', 'Poll question state codes', 'TPollQuestion', 'TPoll');
+
+-- TPollAnswer codes (table_number 1110)
+INSERT INTO TCodeGroup (CodeGroupK, FName, FDescription, FTable, FMainTable) VALUES
+    (11101, 'TPollAnswer.TypeC', 'Poll answer type codes', 'TPollAnswer', 'TPoll'),
+    (11102, 'TPollAnswer.StateC', 'Poll answer state codes', 'TPollAnswer', 'TPoll');
+
+-- Insert a common/default organization with zero key
+INSERT INTO TOrganization (OrganizationK,ParentK,SuperK,CreateD,UpdateD,TypeC,StateC,ClassC,FName,FCode,FDescription,FAddress,FPhone,FEmail,FWebsite,FTaxId,FDeleted)
+VALUES (
+   x'00000000000000000000000000000000',                 -- 16-byte zero key
+   NULL,                                                -- No parent
+   NULL,                                                -- No super
+   datetime('now'),                                     -- Current date/time
+   datetime('now'),                                     -- Current date/time
+   0,                                                   -- Default type (e.g., 1 = Company)
+   0,                                                   -- Default state (e.g., 1 = Active)
+   0,                                                   -- Default class
+   'Default Organization',                              -- Organization name
+   'DEFAULT',                                           -- Organization code
+   'System default organization for unassigned users',  -- Description
+   NULL,                                                -- No address
+   NULL,                                                -- No phone
+   NULL,                                                -- No email
+   NULL,                                                -- No website
+   NULL,                                                -- No tax ID
+   0                                                    -- Not deleted
+);
